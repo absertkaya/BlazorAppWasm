a2e6c4f73d817db8133de3e2192755bb60043aef
Abdullah <abdullah.sertkaya@student.hogent.be>
2020-07-04 17:14:29 +0200

    key branch

---
+++ BlazorApp1/Client/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Client.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
