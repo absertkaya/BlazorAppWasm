326aee7bf35ac96765c1975680a23eba4f9722e9
Abdullah <abdullah.sertkaya@student.hogent.be>
2020-08-01 14:16:29 +0200

    commit

--- BlazorApp1/Server/Startup.cs
+++ BlazorApp1/Server/Startup.cs
@@ -25,6 +25,7 @@ namespace BlazorApp1.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
